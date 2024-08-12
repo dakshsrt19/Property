7e2c86eaaba4f317aae3d6204ccc11d6e35fb0d7
github_dakshsrt19 <dakshsr.t19@gmail.com>
2024-08-12 19:20:54 +0530

    added property-list

--- ui/src/app/property/property-list/property-list.component.ts
+++ ui/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
-  styleUrls: ['./property-list.component.css']
+  styleUrls: ['./property-list.component.css'],
 })
-export class PropertyListComponent {
-
+export class PropertyListComponent implements OnInit {
+  properties: Array<any> = [
+    {
+      Id: 1,
+      Name: 'RK House',
+      Type: 'House',
+      Price: 12000,
+    },
+    {
+      Id: 2,
+      Name: 'Daksh House',
+      Type: 'Duplex House',
+      Price: 120000,
+    },
+    {
+      Id: 3,
+      Name: 'Saru House',
+      Type: 'Duplex House',
+      Price: 120000,
+    },
+    {
+      Id: 4,
+      Name: 'Xyz House',
+      Type: 'Duplex House',
+      Price: 120000,
+    },
+    {
+      Id: 5,
+      Name: 'ABC House',
+      Type: 'Duplex House',
+      Price: 120000,
+    },
+  ];
+  constructor() {}
+  ngOnInit(): void {}
 }
