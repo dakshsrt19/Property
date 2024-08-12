7944ff5b2e08286bdc0afc6d53879cc44c435b46
github_dakshsrt19 <dakshsr.t19@gmail.com>
2024-08-12 20:50:33 +0530

    services added

--- ui/src/app/property/property-list/property-list.component.ts
+++ ui/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from '../../services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -6,56 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
-  properties: Array<any> = [
-    {
-      Id: 1,
-      Name: 'RK House',
-      Type: 'House',
-      Price: 12000,
-    },
-    {
-      Id: 2,
-      Name: 'Daksh House',
-      Type: 'Duplex House',
-      Price: 120000,
-    },
-    {
-      Id: 3,
-      Name: 'Saru House',
-      Type: 'Duplex House',
-      Price: 120000,
-    },
-    {
-      Id: 4,
-      Name: 'Xyz House',
-      Type: 'Duplex House',
-      Price: 120000,
-    },
-    {
-      Id: 5,
-      Name: 'ABC House',
-      Type: 'Duplex House',
-      Price: 120000,
-    },
-    {
-      Id: 6,
-      Name: 'CCC House',
-      Type: 'Duplex House',
-      Price: 120000,
-    },
-    {
-      Id: 7,
-      Name: 'AAA House',
-      Type: 'Duplex House',
-      Price: 120000,
-    },
-    {
-      Id: 8,
-      Name: 'BBB House',
-      Type: 'Duplex House',
-      Price: 120000,
-    },
-  ];
-  constructor() {}
-  ngOnInit(): void {}
+  properties: any = [];
+  constructor(private HousingService: HousingService) {}
+  ngOnInit(): void {
+    this.HousingService.getAllProperties().subscribe(
+      data=>{
+        this.properties=data;
+        console.log(data);
+      }
+    )
+  }
 }
